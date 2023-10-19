22b2e2033718c7674e2aff80892c8e4af02141b3
rakshitsoni05 <rakshitsoni5@gmail.com>
2023-10-19 19:41:43 -0400

    JavaScript and React.js Assignment 3

--- src/Kanbas/Courses/index.js
+++ src/Kanbas/Courses/index.js
@@ -1,41 +1,60 @@
-import db from "../../Kanbas/Database";
-import { Navigate, Route, Routes, useParams } from "react-router-dom";
-import CourseNavigation from "./CourseNavigation";
-import Modules from "./Modules";
-import Home from "./Home";
-import Assignments from "./Assignments";
-import AssignmentEditor from "./Assignments/AssignmentEditor";
-import Grades from "./Grades";
-import "./index.css";
+import React, { useState } from 'react';
+import { Navigate, Route, Routes, useParams, useLocation } from 'react-router-dom';
 
+import CourseNavigation from './CourseNavigation';
+import Modules from './Modules';
+import Home from './Home';
+import Assignments from './Assignments';
+import AssignmentEditor from './Assignments/AssignmentEditor';
+import Grades from './Grades';
+import './index.css';
+import db from "../Database";
 
 function Courses() {
     const { courseId } = useParams();
     const course = db.courses.find((course) => course._id === courseId);
+    const [currentPage, setCurrentPage] = useState('home');
+    const location = useLocation();
+
+    React.useEffect(() => {
+        if (location.pathname.endsWith('/Modules')) {
+            setCurrentPage('Modules');
+        } else if (location.pathname.endsWith('/Assignments')) {
+            setCurrentPage('Assignments');
+        } else if (location.pathname.includes('/Assignments/')) {
+            setCurrentPage('Assignment Editor');
+        } else if (location.pathname.endsWith('/Grades')) {
+            setCurrentPage('Grades');
+        }
+            else if (location.pathname.endsWith('/Home')) {
+                setCurrentPage('Home');
+        } else {
+            setCurrentPage('');
+        }
+    }, [location.pathname]);
+
     return (
         <div>
-
-            <h6 style={{ color: "red" }}>{course.number} {course.name}</h6>
-
-            <CourseNavigation />
+            <b style={{ color: 'red' }}>{course.number} {course.name}</b>
+            { ` > ${currentPage}`}
+            <CourseNavigation onPageChange={setCurrentPage} />
 
             <div>
                 <div
                     className="overflow-y-scroll position-fixed bottom-0 end-0"
                     style={{
-                        left: "320px",
-                        top: "50px",
+                        left: '320px',
+                        top: '50px',
                     }}
                 >
-
                     <Routes>
                         <Route path="/" element={<Navigate to="Home" />} />
-                        <Route path="Home" element={<Home/>} />
-                        <Route path="Modules" element={<Modules/>} />
-                        <Route path="Assignments" element={<Assignments/>} />
+                        <Route path="Home" element={<Home />} />
+                        <Route path="Modules" element={<Modules />} />
+                        <Route path="Assignments" element={<Assignments />} />
                         <Route
                             path="Assignments/:assignmentId"
-                            element={<AssignmentEditor/>}
+                            element={<AssignmentEditor />}
                         />
                         <Route path="Grades" element={<Grades />} />
                     </Routes>

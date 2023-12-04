77a77e16e66c2f9b6d1ce92b70eacce9f72afbce
rakshitsoni05 <rakshitsoni5@gmail.com>
2023-12-04 17:19:41 -0500

    render issues

--- src/Kanbas/Courses/index.js
+++ src/Kanbas/Courses/index.js
@@ -12,7 +12,7 @@ import axios from "axios";
 
 function Courses() {
     const { courseId } = useParams();
-    const URL = "http://localhost:4000/api/courses";
+    const URL = "https://kanbas-node-server-app-9z8p.onrender.com/api/courses";
     const [course, setCourse] = useState({});
     const findCourseById = async (courseId) => {
         const response = await axios.get(

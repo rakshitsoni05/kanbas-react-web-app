4196d5516040b3000d86c05c3fa60ade668e4abc
rakshitsoni05 <rakshitsoni5@gmail.com>
2023-12-04 19:41:54 -0500

    mongodb integration

--- src/Kanbas/users/client.js
+++ src/Kanbas/users/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:4000";
+export const BASE_URL = process.env.REACT_APP_BASE_API_URL;
 export const USERS_URL = `${BASE_URL}/api/users`;
 const request = axios.create({
                                  withCredentials: true,
